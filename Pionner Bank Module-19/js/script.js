987e55ebb92bdc6e1d6045127a0810f4133f5f8b
Coffee-Ovi <ovijit1hsc@gmail.com>
2021-01-27 22:38:10 +0600

    add deposite and balance

--- Pionner Bank Module-19/js/script.js
+++ Pionner Bank Module-19/js/script.js
@@ -1,3 +1,5 @@
+//login button even handler
+
 const loginBtn = document.getElementById('login');
 loginBtn.addEventListener('click', function(){
     const hideLoginArea = document.getElementById('login-area');
@@ -5,3 +7,52 @@ loginBtn.addEventListener('click', function(){
     const transactionArea = document.getElementById("transaction-area");
     transactionArea.style.display = "block";
 })
+
+//    deposite buytton even handler
+
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function(){
+    const depositeNumber = getInputNumber("depositAmount");
+    
+//    put deposite amount in the input and show and add them
+    
+//    const currentDeposit = document.getElementById("currentDeposit").innerText;
+//    const currentDepositeNumber = parseFloat(currentDeposit);
+//    const totalDeposite = depositNumber + currentDepositeNumber;
+//    document.getElementById("currentDeposit").innerText = totalDeposite;
+//   
+    updateSpanText("currentDeposit", depositNumber);
+    updateSpanText("currentBalance", depositNumber);
+    
+    document.getElementById("depositAmount").value = "";
+    
+//    add the current deposite with the current balance and show them
+    
+//    const currentBalance = document.getElementById('currentBalance').innerText;
+//    const currentBalanceNumber = parseFloat(currentBalance);
+//    const totalBalance = depositNumber + currentBalanceNumber;
+//    document.getElementById("currentBalance").innerText = totalBalance;
+})
+
+//        withdrow button handler
+const withdrawBtn = document.getElementById('addWithdraw');
+withdrawBtn.addEventListener("click", function(){
+    const withdrawNumber = getInputNumber("withdrawAmount");
+    console.log(withdrawNumber);
+})
+
+function getInputNumber(id){
+    const amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(amount);
+    return amountNumber;
+}
+
+
+
+
+function updateSpanText(id, depositNumber){
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = depositNumber + currentNumber;
+    document.getElementById(id).innerText = totalAmount;
+}
